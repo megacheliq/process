96296a703705197df215d2cc5727ef31fc26c131
m.bolotov <m.bolotov@element-it.ru>
2024-10-19 22:58:45 +0500

    zalupa

--- process.client/src/root/axios-client.ts
+++ process.client/src/root/axios-client.ts
@@ -1,7 +1,19 @@
-import axios from "axios";
+import axios from 'axios';
+import https from 'https';
+
+const httpsAgent = new https.Agent({
+  rejectUnauthorized: false,
+});
 
 const axiosClient = axios.create({
-    baseURL: 'https://82.146.36.64:7165/api/'
+  baseURL: 'https://82.146.36.64:7165/api/',
+});
+
+axiosClient.interceptors.request.use(config => {
+  config.httpsAgent = httpsAgent;
+  return config;
+}, error => {
+  return Promise.reject(error);
 });
 
 export default axiosClient;
